17cc74f9cbb1508619e0da2e5bfebe550df57ebe
lzwjava <lzwjava@gmail.com>
2023-12-13 05:33:16 +0800

    add

--- detect_name.py
+++ detect_name.py
@@ -1,4 +1,33 @@
 import hanlp
 
 HanLP = hanlp.load(hanlp.pretrained.mtl.CLOSE_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_SMALL_ZH)
-HanLP(['2021年HanLPv2.1为生产环境带来次世代最先进的多语种NLP技术。', '阿婆主来到北京立方庭参观自然语义科技公司。'])
+
+import os
+
+directory_path = "./_posts"
+
+files = sorted(os.listdir(directory_path))
+
+for file in files:
+    file_path = os.path.join(directory_path, file)
+    with open(file_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+        # print(f'content length: {len(content)}')
+
+        print(f'{f.name}')
+
+        paragraphs = content.split('\n\n')
+
+        for paragraph in paragraphs:
+            # print(f'paragraph length: {len(paragraph)}')
+
+            result = HanLP(paragraph)
+            # print(result)
+
+            pku = result['ner/pku']
+
+            for i in range(len(pku)):
+                item = pku[i]
+                if item[1] == 'nr':
+                    print(item)
