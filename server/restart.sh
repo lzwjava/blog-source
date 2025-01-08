c804556e75269f6f9ab76c24218ac2c63a65d068
Zhiwei Li <lzwjava@gmail.com>
2025-01-09 07:20:31 +0800

    add

---
+++ server/restart.sh
@@ -0,0 +1,12 @@
+wget https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh
+
+chmod +x install_server.sh
+
+./install_server.sh
+
+sudo systemctl start nginx
+
+tmux 
+
+# change name cheap
+
