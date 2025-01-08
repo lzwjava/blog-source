8a96a832b53112b1d1a6c77beee3a3bf326e644c
Zhiwei Li <lzwjava@gmail.com>
2025-01-09 04:15:40 +0800

    add

---
+++ server/bandwidth_auto.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install vnstat
+sudo apt install python3-pip
+pip install Flask
+sudo apt install emacs-nox
+pip3 install flask_cors
+sudo apt install nginx
+sudo apt install certbot python3-certbot-nginx
+
