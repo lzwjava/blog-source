7a997ca694292242dc04019c0c0c74bcc80b65f3
Zhiwei Li <lzwjava@gmail.com>
2025-01-14 14:11:07 +0800

    refactor(js): split audio and PDF handlers into separate files

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -20,21 +20,5 @@ document.addEventListener("DOMContentLoaded", function () {
             audioPlayer.play();
         });
     }
-
-    var downloadPdfButton = document.getElementById('downloadPdfButton');
-    if (downloadPdfButton) {
-        downloadPdfButton.addEventListener('click', function (event) {
-            event.preventDefault(); // Prevent default anchor behavior
-
-            // Extract the base filename without the extension
-            var filePath = downloadPdfButton.getAttribute('data-file-path'); // e.g., "pages/donate-en.md"
-            var fileName = filePath.split('/').pop().replace('.md', ''); // e.g., "donate-en"
-
-            // Set the PDF source to the corresponding PDF file
-            var pdfFile = '/assets/pdfs/' + fileName + '.pdf'; // e.g., "/assets/pdfs/donate-en.pdf"
-
-            // Initiate the download or open the PDF in a new tab
-            window.open(pdfFile, '_blank');
-        });
-    }
+   
 });
