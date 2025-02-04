9496d13520a192b99d5236780e3e9edd1d51fc07
Zhiwei Li <lzwjava@gmail.com>
2025-02-04 20:16:38 +0800

    chore: remove old hello scripts

--- scripts/hello.s
+++ scripts/hello.s
@@ -2,18 +2,16 @@
 .text
 
 _start:
-    // Write "Hello, World!" to stdout
-    mov x8, #64             // syscall number for write (sys_write)
-    mov x0, #1              // file descriptor 1 (stdout)
-    adrp x1, msg@PAGE       // load the page address of msg
-    add x1, x1, msg@PAGEOFF // add the offset within the page
-    mov x2, #14             // length of the message
-    svc #0                   // invoke syscall
+    mov x8, #64
+    mov x0, #1
+    adrp x1, msg@PAGE
+    add x1, x1, msg@PAGEOFF
+    mov x2, #14
+    svc #0
 
-    // Exit the program
-    mov x8, #93             // syscall number for exit (sys_exit)
-    mov x0, #0              // exit code 0
-    svc #0                   // invoke syscall
+    mov x8, #93
+    mov x0, #0
+    svc #0
 
 .data
 msg:
