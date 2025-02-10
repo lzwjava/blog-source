9dd1d4c11066f068043b2e3cfd8d778143647f41
Zhiwei Li <lzwjava@gmail.com>
2025-02-10 14:25:42 +0800

    ci: added hello.c, updated hello.s

--- scripts/hello.s
+++ scripts/hello.s
@@ -1,18 +1,30 @@
-.global _start
-.text
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 15, 0	sdk_version 15, 2
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #32
+	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
+	add	x29, sp, #16
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	mov	w8, #0                          ; =0x0
+	str	w8, [sp, #8]                    ; 4-byte Folded Spill
+	stur	wzr, [x29, #-4]
+	adrp	x0, l_.str@PAGE
+	add	x0, x0, l_.str@PAGEOFF
+	bl	_printf
+	ldr	w0, [sp, #8]                    ; 4-byte Folded Reload
+	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
+	add	sp, sp, #32
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_.str:                                 ; @.str
+	.asciz	"Hello, World!\n"
 
-_start:
-    mov x8, #64
-    mov x0, #1
-    adrp x1, msg@PAGE
-    add x1, x1, msg@PAGEOFF
-    mov x2, #14
-    svc #0
-
-    mov x8, #93
-    mov x0, #0
-    svc #0
-
-.data
-msg:
-    .asciz "Hello, World!\n"
+.subsections_via_symbols
