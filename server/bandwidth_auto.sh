2e53a47e372a9b929068f1ace7a00db404d94a04
Zhiwei Li <lzwjava@gmail.com>
2025-02-13 13:17:02 +0800

    chore: remove server scripts

--- server/bandwidth_auto.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update
-sudo apt-get install vnstat
-sudo apt install python3-pip
-pip install Flask
-sudo apt install emacs-nox
-pip3 install flask_cors
-sudo apt install nginx
-sudo apt install certbot python3-certbot-nginx -y
