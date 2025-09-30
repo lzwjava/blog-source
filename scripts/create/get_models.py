96c4b9f2e04cac07040fc16f9e4933b302a96154
Zhiwei Li <lzwjava@gmail.com>
2025-10-01 05:28:08 +0800

    refactor(scripts): return model keys as JSON array

--- scripts/create/get_models.py
+++ scripts/create/get_models.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 import os
 import sys
 
@@ -7,9 +8,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".
 from scripts.llm.openrouter_client import MODEL_MAPPING
 
 def get_models():
-    """Return comma-separated list of model keys"""
+    """Return JSON array of model keys"""
     model_keys = sorted(MODEL_MAPPING.keys())
-    print(",".join(model_keys))
+    print(json.dumps(model_keys))
 
 if __name__ == "__main__":
     get_models()
