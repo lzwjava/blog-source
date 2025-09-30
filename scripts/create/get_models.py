9d1aa9004ce378c89659cc2145f103c09ad8c3a0
Zhiwei Li <lzwjava@gmail.com>
2025-10-01 05:32:24 +0800

    refactor(scripts): simplify get_models output format

--- scripts/create/get_models.py
+++ scripts/create/get_models.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import json
 import os
 import sys
 
@@ -8,9 +7,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".
 from scripts.llm.openrouter_client import MODEL_MAPPING
 
 def get_models():
-    """Return JSON array of model keys"""
+    """Return comma-separated list of model keys"""
     model_keys = sorted(MODEL_MAPPING.keys())
-    print(json.dumps(model_keys))
+    print(",".join(model_keys))
 
 if __name__ == "__main__":
     get_models()
