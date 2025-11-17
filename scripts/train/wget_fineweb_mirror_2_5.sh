35b445d5a6c037d8ac4b29994eb020d396cc059d
lzwjava <lzwjava@gmail.com>
2025-11-18 00:21:29 +0800

    feat(train): add CC-MAIN-2013-20 parquet files 6–10

--- scripts/train/wget_fineweb_mirror_2_5.sh
+++ scripts/train/wget_fineweb_mirror_2_5.sh
@@ -13,3 +13,13 @@ wget -c "https://hf-mirror.com/datasets/HuggingFaceFW/fineweb/resolve/main/data/
 wget -c "https://hf-mirror.com/datasets/HuggingFaceFW/fineweb/resolve/main/data/CC-MAIN-2013-20/000_00004.parquet?download=true"
 
 wget -c "https://hf-mirror.com/datasets/HuggingFaceFW/fineweb/resolve/main/data/CC-MAIN-2013-20/000_00005.parquet?download=true"
+
+wget -c "https://hf-mirror.com/datasets/HuggingFaceFW/fineweb/resolve/main/data/CC-MAIN-2013-20/000_00006.parquet?download=true"
+
+wget -c "https://hf-mirror.com/datasets/HuggingFaceFW/fineweb/resolve/main/data/CC-MAIN-2013-20/000_00007.parquet?download=true"
+
+wget -c "https://hf-mirror.com/datasets/HuggingFaceFW/fineweb/resolve/main/data/CC-MAIN-2013-20/000_00008.parquet?download=true"
+
+wget -c "https://hf-mirror.com/datasets/HuggingFaceFW/fineweb/resolve/main/data/CC-MAIN-2013-20/000_00009.parquet?download=true"
+
+wget -c "https://hf-mirror.com/datasets/HuggingFaceFW/fineweb/resolve/main/data/CC-MAIN-2013-20/000_00010.parquet?download=true"
